a70f732c75701cf95b6978b1cda18f7f682ad780
Sean <seanpoulter@hotmail.com>
2022-07-09 02:04:32 -0400

    Finish sum all.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+    let sum = 0
+    if (num1 < 0 || num2 < 0 || Number.isInteger(num1) === false || Number.isInteger(num2) === false) {
+        return "ERROR"
+    }
+    else if (num1 < num2) {
+        for (num1; num1 <= num2; num1++) {
+            if (num1 <= num2) {
+                sum += num1;
+            }
+        }
+    }
+    else if (num2 < num1) {
+        for (num1; num1 >= num2; num1--) {
+            if (num1 >= num2) {
+                sum += num1;
+            }
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
