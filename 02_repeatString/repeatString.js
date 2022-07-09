b1dea1891f478c39daf1bb1ffc1467adad0c6e38
Sean <seanpoulter@hotmail.com>
2022-07-08 22:51:40 -0400

    Finish repeatstring lesson.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+let num = 0
+let string
+let i = 0
+let final
 
+const repeatString = function (string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+
+    for (i = 0; i < num; i++); {
+        final = string.repeat(num)
+    }
+
+    return final
 };
 
 // Do not edit below this line
