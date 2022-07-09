b7946e0587dba0418280feaec5394f9b79b68738
Sean <seanpoulter@hotmail.com>
2022-07-08 23:42:36 -0400

    Finish reverse string.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+    let final = ""
+    for (let i = str.length; i > 0; i--) {
+        final += str.substr(i -1, 1);
+    }
+    return final
 };
 
 // Do not edit below this line
