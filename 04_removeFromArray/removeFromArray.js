c2f87790dc75d6f1ba142c427f1bfce6738f7bfe
Sean <seanpoulter@hotmail.com>
2022-07-09 01:19:28 -0400

    Finish remove from array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+    for (let i = 0; i < array.length; i++) {
+        if (args.includes(array[i])) {
+            array.splice(i, 1);
+            i--;
+        }
+    } 
+    return array;
 };
 
 // Do not edit below this line
