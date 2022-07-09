b9d587a23dba11f17b0018dca1acb17e999c034b
Sean <seanpoulter@hotmail.com>
2022-07-09 02:39:35 -0400

    Finish celcius and farenheit calculator.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function (ftoc) {
+  let cel = (ftoc - 32) * 5 / 9
+  return Math.round(cel * 10) / 10
 };
 
-const ctof = function() {
-
+const ctof = function (ctof) {
+  let far = ctof  / 5 * 9 +32
+  return Math.round(far * 10) / 10
 };
 
 // Do not edit below this line
