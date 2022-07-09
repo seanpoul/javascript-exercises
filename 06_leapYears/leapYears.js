c4778df67c1aa9f5fd96898cfd308a611ac053de
Sean <seanpoulter@hotmail.com>
2022-07-09 02:29:04 -0400

    Finish leap year.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function (leapYear) {
+    if (leapYear % 100 == 0 && !(leapYear % 400 == 0)) {
+        return false
+    }
+    else if (leapYear % 4 == 0) {
+        return true
+    }
+    else {
+        return false
+    }
 };
 
 // Do not edit below this line
